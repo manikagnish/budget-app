51e478b24298f794bb1c04a0108cac14819514b4
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-14 12:52:28 +0530

    cleaned up files and installed dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Badge } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello <Badge bg="secondary">world</Badge>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
