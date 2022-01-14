834d691b50a9615beaf7a1bbd90899c05504f136
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-14 14:21:20 +0530

    created BudgetCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { Badge } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/container";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello <Badge bg="secondary">world</Badge>
-      </h1>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expenses</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard name="Entertainment" gray amount={200} max={1000} />
+      </div>
+    </Container>
   );
 }
 
