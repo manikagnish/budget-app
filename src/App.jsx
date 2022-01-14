f84b62e2ceb3aa10252bafb231f2289db68cd8c9
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-14 18:51:53 +0530

    created uncategorized budget card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import BudgetCard from "./components/BudgetCard";
 import { useState } from "react";
 import { useBudgets } from "./contexts/BudgetContext";
 import AddExpenseModal from "./components/AddExpenseModal";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
 
 function App() {
   const { budgets, getBudgetExpenses } = useBudgets();
@@ -52,6 +53,7 @@ function App() {
               />
             );
           })}
+          <UncategorizedBudgetCard />
         </div>
       </Container>
       <AddBudgetModal
